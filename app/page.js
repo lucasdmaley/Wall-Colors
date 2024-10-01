0c56fa346ed83c8004e5ad1764d081bbf9860232
Lucas Maley <lucasdmaley@gmail.com>
2024-10-01 00:48:55 -0400

    color picker and text inputs

--- app/page.js
+++ app/page.js
@@ -5,33 +5,66 @@ import styles from "./page.module.css";
 import { Inter } from 'next/font/google';
 import { ChromePicker } from 'react-color';
 
+import { CustomPicker } from 'react-color';
+
+var { Saturation } = require('react-color/lib/components/common');
+var { Hue } = require('react-color/lib/components/common');
+var { EditableInput } = require('react-color/lib/components/common');
+
+
 import React, { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
+// Define your custom picker component
+const MyPicker = ({ hex, hsl, hsv, onChange }) => {
+  const [hexPickerState, setHexPickerState] = useState({ color: "#757575" });
+
+  const hexInputStyle = {
+    input: { border: "none", fontSize: '24px', color: hexPickerState.color, backgroundColor: "rgba(0,0,0,0)", width: "120px" },
+  }
+
+  return (
+    <div style={{ width: '420px' }} onClick={() => setHexPickerState({color: "#000"})}>
+      
+      {/* Saturation Area */}
+      <div style={{ position: 'relative', width: '100%', paddingBottom: '75%', marginBottom: '10px' }}>
+        <Saturation hsl={hsl} hsv={hsv} onChange={onChange} />
+      </div>
+
+      {/* Hue Area */}
+      <div style={{ position: 'relative', height: '10px', marginBottom: '10px' }}>
+        <Hue hsl={hsl} onChange={onChange} />
+      </div>
+
+      {/* Editable Hex Input */}
+      <div className={styles.row}>
+        <p className={styles.inputName}>Hex Code</p>
+        <div className={styles.hexPickerWrapper} onClick={() => setHexPickerState({color: "#000"})}>
+          <EditableInput value={hex} onChange={onChange} style={hexInputStyle} className={styles.hexPicker}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const CustomColorPicker = CustomPicker(MyPicker);
+
 
 export default function Home() {
-  const [state, setState] = useState({
+  const [colorState, setColorState] = useState({
     displayColorPicker: true,
-    color: "#ab10ed"
+    color: "#208FFF"
   });
 
-  /* Color Picker component */
-  const ColorPicker = () => {
-    const handleChange = (color) => {
-      setState({...state, color: color.hex });
-    };
+  const [username, setUsername] = useState("");
+  const [colorName, setColorName] = useState("");
 
-    return (
-      <div>
-        { state.displayColorPicker ?
-          <div className={ styles.popover }>
-            <ChromePicker color={state.color} onChange={ handleChange } />
-          </div> : null
-        }
-      </div>
-    )
-  }
+  /* Color Picker component */
+  const handleChange = (newColor) => {
+    setColorState({ ...colorState, color: newColor.hex});
+  };
+
 
 
   return (
@@ -40,17 +73,30 @@ export default function Home() {
         <main className={styles.main}>
           <div className={styles.leftSide}>
             <p className={styles.title}>Hey!</p>
-            <p className={styles.bodyText}>I’m trying to add some color to my room, so please choose yours + submit your name and I’ll add it to the wall :)</p>
-            <ColorPicker />
+            <p className={styles.bodyText}>I’m trying to add some color to my room, so please choose your favorite + submit your name and I’ll add it to the wall :)</p>
+            <CustomColorPicker 
+              color={colorState.color} 
+              onChange={handleChange} 
+            />
+            <div className={styles.row}>
+            <p className={styles.inputName}>Color Name</p>
+              <input type="text" value={colorName} onChange={(e) => {setColorName(e.target.value)}}
+                className={styles.inputUsername} placeholder="Chill Blue"/>
+            </div>
+            <div className={styles.row}>
+            <p className={styles.inputName}>Your Name</p>
+              <input type="text" value={username} onChange={(e) => {setUsername(e.target.value)}}
+                className={styles.inputUsername} placeholder="Lucas Maley"/>
+            </div>
           </div>
           <div className = {styles.rightSide}>
             <div className={styles.colorSwatch}>
-              <div style={{backgroundColor: state.color}} className={styles.colorItself}/>
+              <div style={{backgroundColor: colorState.color}} className={styles.colorItself}/>
               <div className={styles.swatchText}>
-                <p className={styles.swatchTextName}>Lucas Maley</p>
+                <p className={styles.swatchTextName}>{username == "" ? "Lucas Maley" : username}</p>
                 <div className={styles.swatchTextWrapper}>
-                  <p className={styles.swatchTextColor}>{state.color}</p>
-                  <p>09/24/2001</p>
+                  <p className={styles.swatchTextColor}>{colorState.color.toUpperCase()}</p>
+                  <p>"{colorName == "" ? "Chill Blue" : colorName}"</p>
                 </div>
               </div>
             </div>
@@ -62,90 +108,13 @@ export default function Home() {
   );
 }
 
+
 /*
 
-<Image
-  className={styles.logo}
-  src="https://nextjs.org/icons/next.svg"
-  alt="Next.js logo"
-  width={180}
-  height={38}
-  priority
-/>
-<ol>
-  <li>
-    Get started by editing <code>app/page.js</code>.
-  </li>
-  <li>Save and see your changes instantly.</li>
-</ol>
-
-<div className={styles.ctas}>
-  <a
-    className={styles.primary}
-    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Image
-      className={styles.logo}
-      src="https://nextjs.org/icons/vercel.svg"
-      alt="Vercel logomark"
-      width={20}
-      height={20}
-    />
-    Deploy now
-  </a>
-  <a
-    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-    className={styles.secondary}
-  >
-    Read our docs
-  </a>
-</div>
-
-
- <a
-  href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <Image
-    aria-hidden
-    src="https://nextjs.org/icons/file.svg"
-    alt="File icon"
-    width={16}
-    height={16}
-  />
-  Learn
-</a>
-<a
-  href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <Image
-    aria-hidden
-    src="https://nextjs.org/icons/window.svg"
-    alt="Window icon"
-    width={16}
-    height={16}
-  />
-  Examples
-</a>
-<a
-  href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <Image
-    aria-hidden
-    src="https://nextjs.org/icons/globe.svg"
-    alt="Globe icon"
-    width={16}
-    height={16}
-  />
-  Go to nextjs.org →
-</a>
+<p 
+                className={styles.displayText} 
+                onClick={() => document.getElementById('username').focus()} // Click to focus input
+              >
+                {username || "Lucas Maley"}
+                </p>
 */
