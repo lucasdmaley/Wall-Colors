f5c0f5f64f273a32a1137f0d16ef7208788342b5
Lucas Maley <lucasdmaley@gmail.com>
2024-10-03 01:28:34 -0400

    submit button states and nicer styling

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/page.module.css";
 import { Inter } from 'next/font/google';
 
 import { CustomPicker } from 'react-color';
+import { FaArrowRight } from "react-icons/fa";
 
 var { Saturation } = require('react-color/lib/components/common');
 var { Hue } = require('react-color/lib/components/common');
@@ -52,6 +53,8 @@ export default function Home() {
   const [username, setUsername] = useState("");
   const [colorName, setColorName] = useState("");
   const [hexCode, setHexCode] = useState("#208FFF");
+  const [submitState, setSubmitState] = useState(0); // 0 == unsubmitted; 1 == submitting; 2 == submitted
+  const [remindUsername, setRemindUsername] = useState(false);
 
   /* Color Picker component */
   const handleChange = (newColor) => {
@@ -59,23 +62,29 @@ export default function Home() {
   };
 
   const postSwatch = async () => {
-    console.log("in postSwatch!")
-    const response = await fetch("/api/postSwatch", {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        username: username,
-        color: colorName,
-        hexcode: hexCode,
-      }),
-    }).then((data) => data.json()).then((r) => {
-        console.log("there we go - all done!");
-    });
-    ;
+    if (submitState == 0) {
+      setSubmitState(1);
+      setRemindUsername(false);
+      console.log("in postSwatch!")
+      const response = await fetch("/api/postSwatch", {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          username: username,
+          color: colorName,
+          hexcode: hexCode,
+        }),
+      }).then((data) => data.json()).then((r) => {
+          console.log("there we go - all done!");
+          console.log(r);
+          if (r.message == "Success!") setSubmitState(2);
+          else setSubmitState(3);
+      });
+    }
   }
 
   return (
@@ -85,32 +94,48 @@ export default function Home() {
           <div className={styles.leftSide}>
             <p className={styles.title}>Hey!</p>
             <p className={styles.bodyText}>I’m trying to add some color to my room, so please choose your favorite + submit your name and I’ll add it to the wall :)</p>
-            <CustomColorPicker 
-              color={hexCode} 
-              onChange={handleChange} 
-            />
+            <CustomColorPicker color={hexCode} onChange={handleChange} />
             <div className={styles.row}>
-                <p className={styles.inputName}>Color Name</p>
-                <input className={styles.inputUsername} type="text" value={colorName} onChange={(e) => {setColorName(e.target.value)}} placeholder="Chill Blue"/>
+              <p className={styles.inputName}>Color Name</p>
+              <input className={styles.inputUsername} type="text" value={colorName} onChange={(e) => {setColorName(e.target.value)}} placeholder="Chill Blue"/>
             </div>
             <div className={styles.row}>
-                <p className={styles.inputName}>Your Name</p>
-                <input className={styles.inputUsername}type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} placeholder="Lucas Maley"/>
+              <p className={styles.inputName}>Your Name</p>
+              <input className={styles.inputUsername}type="text" value={username} onChange={(e) => {setUsername(e.target.value)}} placeholder="Lucas Maley"/>
             </div>
-            <div className={styles.squareButton} onClick={() => {postSwatch(); console.log("hey")}}/>
             <div className = {styles.rightSide}>
-            <div className={styles.colorSwatch}>
-              <div style={{backgroundColor: hexCode}} className={styles.colorItself}/>
-              <div className={styles.swatchText}>
-                <p className={styles.swatchTextName}>{username == "" ? "Lucas Maley" : username}</p>
-                <div className={styles.swatchTextWrapper}>
-                  <p className={styles.swatchTextHexcode}>{hexCode.toUpperCase()}</p>
-                  <p className={styles.swatchTextColor}>"{colorName == "" ? "Chill Blue" : colorName}"</p>
+              <div className={styles.colorSwatch}>
+                <div style={{backgroundColor: hexCode}} className={styles.colorItself}/>
+                <div className={styles.swatchText}>
+                    <p className={styles.swatchTextName}>{username == "" ? "Lucas Maley" : username}</p>
+                    <div className={styles.swatchTextWrapper}>
+                    <p className={styles.swatchTextHexcode}>{hexCode.toUpperCase()}</p>
+                    {(username != "" && hexCode != "#208FFF") && colorName == "" ? <></> : <p className={styles.swatchTextColor}>"{colorName == "" ? "Chill Blue" : colorName}"</p>}
+                    </div>
                 </div>
               </div>
+              <p className={styles.caption}>This is what it'll look like</p>
+              {remindUsername ? <p className={styles.warning}>Make sure to add your name before submitting!</p> : <></>}
             </div>
-            <p className={styles.caption}>This is what it'll look like</p>
-          </div>
+            { submitState == 0 ?
+              <div className={styles.submitButton} onClick={() => {username != "" ? postSwatch() : setRemindUsername(true) }  }>
+                <p className={styles.submitText}>Submit</p>
+                <FaArrowRight className={styles.submitText} value={{ color: "white"}}/>
+              </div>
+              : submitState == 1 ?
+              <div className={styles.submitButtonSubmitting}>
+                <p className={styles.submitText}>Submitting...</p>
+              </div>
+              : submitState == 2 ?
+              <div className={styles.submitButtonSuccess}>
+                <p className={styles.submitText}>Submitted! Thank you :)</p>
+              </div>
+              :
+              <div className={styles.submitButtonError}>
+                <p className={styles.submitText}>Error :/</p>
+                <p className={styles.errorText}>Let me know and I'll try and fix it!</p>
+              </div>
+            }
           </div>
         </main>
       </div>
