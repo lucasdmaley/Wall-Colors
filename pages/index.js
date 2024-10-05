80423c27671f1feb7ea3d2507fa3f153ca421927
Lucas Maley <lucasdmaley@gmail.com>
2024-10-05 00:28:22 -0400

    Added Datadog RUM

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,26 @@ import { Inter } from 'next/font/google';
 import { CustomPicker } from 'react-color';
 import { FaArrowRight } from "react-icons/fa";
 
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+  applicationId: '0b185dad-2749-421d-8eb1-b43589c403e4',
+  clientToken: 'pub63c92b03daa327e984d95b898c34b836',
+  // `site` refers to the Datadog site parameter of your organization
+  // see https://docs.datadoghq.com/getting_started/site/
+  site: 'datadoghq.eu',
+  service: 'wall-colors',
+  env: 'prod',
+  // Specify a version number to identify the deployed version of your application in Datadog
+  // version: '1.0.0',
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 100,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'allow',
+});
+
 var { Saturation } = require('react-color/lib/components/common');
 var { Hue } = require('react-color/lib/components/common');
 var { EditableInput } = require('react-color/lib/components/common');
